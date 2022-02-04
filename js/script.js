7bf8f432c623293e8384f7824a9ccd2576d0276a
Adner16 <giovanni.renda16@gmail.com>
2022-02-04 01:37:57 +0100

    added script

--- js/script.js
+++ js/script.js
@@ -5,16 +5,28 @@ Vue.config.devtools= true;
 const app = new Vue({
     el: '#root',
     data: {
+        currentImage: 0,
         images: [
-            {images/image1.jpg},
-            {images/image2.jpg},
-            {images/image3.jpg},
-            {images/image4.jpg},
-        ];
+            'images/image1.jpg',
+            'images/image2.jpg',
+            'images/image3.jpg',
+            'images/image4.jpg',
+        ]
 
-    }
+    },
     methods: {
-
-    
+        isCurrent(index) {
+            return (index == this.currentImage);
+        },
+        setPic(index) {
+           this.currentImage = index;    
+        },
+        nextPic(){
+            this.currentImage++;
+        },
+        prevPic(){
+            this.currentImage--;
+        }
     }
+    
 })
